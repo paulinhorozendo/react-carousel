31b6c57e3b3c1bc97c2e6838f7a52057693f6f18
paulinhorozendo <81476932+paulinhorozendo@users.noreply.github.com>
2022-01-06 10:16:50 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import Carousel from "./components/Carousel";
+
+function App() {
+  return (
+    <div className="App">
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
---
+++ CarouselData.js
@@ -0,0 +1,13 @@
+import Kimetsu from "../Images/kimetsu.jpg";
+import Bleach from "../Images/bleach.png";
+import Fairy from "../Images/fairy.jpg";
+
+export const images = [
+  {
+    title: "Kimetsu no Yaiba",
+    subtitle: "Entertainment District Arc",
+    img: Kimetsu,
+  },
+  { title: "Bleach", subtitle: " The Thousand-Year Blood War", img: Bleach },
+  { title: "Fairy Tail", subtitle: "100 Years Quest", img: Fairy },
+];
